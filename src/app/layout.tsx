ec590511d32f22d00519e360518115a7dcce0948
sverg84 <134344913+sverg84@users.noreply.github.com>
2024-03-09 17:52:37 -0800

    nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Flowbite, ThemeModeScript } from "flowbite-react";
-import Nav from "./components/nav";
+import Nav from "./components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
