0bb49eadbc2118a52761d8027556a51d2fa0973a
sverg84 <134344913+sverg84@users.noreply.github.com>
2024-03-14 17:30:36 -0700

    reverse exp timeline

--- src/app/components/timelines/experience.tsx
+++ src/app/components/timelines/experience.tsx
@@ -4,6 +4,28 @@ import TimelineBase from "./base";
 
 const items: Item[] = [
   {
+    body: `Full-stack Web product software engineer for the\
+    Org Effectiveness team within the Enterprise Engineering division.\
+    Released multiple high-priority stakeholder feature requests\
+    pertaining to HR-sensitive employee data. Leader of team app\
+    performance optimization opportunities.`,
+    href: "https://about.meta.com/",
+    id: "meta-swe",
+    skills: ["React", "Hack", "GraphQL", "Relay", "MySQL", "Jest"],
+    time: "2020-2023",
+    title: "Software Engineer at Meta Platforms, Inc.",
+  },
+  {
+    body: `Intern for the Traffic Analytics team within the Infrastructure\
+    division. Created monitoring tool for team oncall to measure network\
+    packet failures with interactive web page.`,
+    href: "https://about.meta.com/",
+    id: "fb-intern",
+    skills: ["C++", "Python", "Thrift", "MySQL"],
+    time: "Summer 2019",
+    title: "Software Engineer Intern at Facebook",
+  },
+  {
     body: "Assistant at the university's Transportation Research Institute as\
         part of the Undergraduate Research Opportunity Program. Conducted an\
         anthropometric study for Defence Research & Development Canada\
@@ -15,28 +37,6 @@ const items: Item[] = [
     skills: ["MATLAB", "Wolfram Mathematica"],
     time: "2016-2017",
     title: "Research Assistant at the University of Michigan",
-  },
-  {
-    body: `Intern for the Traffic Analytics team within the Infrastructure\
-          division. Created monitoring tool for team oncall to measure network\
-          packet failures with interactive web page.`,
-    href: "https://about.meta.com/",
-    id: "fb-intern",
-    skills: ["C++", "Python", "Thrift", "MySQL"],
-    time: "Summer 2019",
-    title: "Software Engineer Intern at Facebook",
-  },
-  {
-    body: `Full-stack Web product software engineer for the\
-          Org Effectiveness team within the Enterprise Engineering division.\
-          Released multiple high-priority stakeholder feature requests\
-          pertaining to HR-sensitive employee data. Leader of team app\
-          performance optimization opportunities.`,
-    href: "https://about.meta.com/",
-    id: "meta-swe",
-    skills: ["React", "Hack", "GraphQL", "Relay", "MySQL", "Jest"],
-    time: "2020-2023",
-    title: "Software Engineer at Meta Platforms, Inc.",
   },
 ];
 
