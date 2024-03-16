0b85e6fe76f5df3267f49072bc5eb3ba070919e2
sverg84 <134344913+sverg84@users.noreply.github.com>
2024-03-15 18:09:27 -0700

    pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Flowbite, ThemeModeScript } from "flowbite-react";
@@ -8,6 +8,13 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#000000" },
+    { media: "(prefers-color-scheme: light)", color: "rgb(214, 219, 220)" },
+  ],
+};
 
 export const metadata: Metadata = {
   title: "Stephen Vergara",
---
+++ src/app/manifest.ts
@@ -0,0 +1,34 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Stephen Vergara",
+    short_name: "sverg",
+    description: "Stephen's porfolio, powered by Next.js",
+    display: "standalone",
+    start_url: "/",
+    icons: [
+      {
+        purpose: "maskable",
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      {
+        purpose: "any",
+        src: "/react.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    screenshots: [
+      // @ts-expect-error Manifest needs "form_factor" to distinguish
+      // mobile vs. desktop despite the type definition not including it.
+      { form_factor: "wide", sizes: "2880x1626", src: "/desktop.png" },
+      // @ts-expect-error see above
+      { form_factor: "narrow", sizes: "666x1452", src: "/mobile.png" },
+    ],
+    background_color: "black",
+    theme_color: "black",
+  };
+}
