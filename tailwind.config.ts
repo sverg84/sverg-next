c9083b5422ca53a500dc60604ae7933e8b0bafe4
sverg84 <134344913+sverg84@users.noreply.github.com>
2025-01-15 14:19:58 -0800

    upgrade flowbite and flowbite-react

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
 import type { Config } from "tailwindcss";
 
-import flowbite from "flowbite/plugin";
+import flowbite from "flowbite-react/tailwind";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -52,6 +52,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [flowbite],
+  plugins: [flowbite.plugin()],
 };
 export default config;
